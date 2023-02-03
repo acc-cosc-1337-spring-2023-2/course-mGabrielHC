61af7ed56f9cf3a79d58b740956b3b7d6899bc15
mGabrielHC <123131838+mGabrielHC@users.noreply.github.com>
2023-02-03 07:12:52 +0000

    modified:   src/examples/00_hello_world/main.cpp

--- src/examples/01_data_types/02_decimals/main.cpp
+++ src/examples/01_data_types/02_decimals/main.cpp
@@ -1,5 +1,17 @@
+#include "decimals.h"// tell C++ where you find add to double 1 function
+#include <iostream>
+
+using std::cout;//explictly tell C++ we are only using cout from iostream and std
+
 int main()
 {
+    double num = 0;
+    double num1 = add_to_double_1(num);
+
+    //we do something
+    num1 = num1 + 1;
+
+    cout<<"Adding to a double:"<<num1<<"\n";
 
     return 0;
 }
