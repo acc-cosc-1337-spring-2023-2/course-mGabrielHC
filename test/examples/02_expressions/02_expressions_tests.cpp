6c2a487c7523fa78496d2d0767fd41b796ffb0a5
mGabrielHC <123131838+mGabrielHC@users.noreply.github.com>
2023-02-10 19:12:06 +0000

    .

--- test/examples/02_expressions/02_expressions_tests.cpp
+++ test/examples/02_expressions/02_expressions_tests.cpp
@@ -2,6 +2,7 @@
 #include "catch.hpp"
 #include "expressions.h"
 #include "constants.h"
+#include "casting.h"
 TEST_CASE("Verify Test Configuration", "verification") {
 	REQUIRE(true == true);
 }
@@ -15,14 +16,16 @@ TEST_CASE("Verify operator_precedence_2", "validating order of operations") {
 	REQUIRE(operator_precedence_2(3,6,3) == 3);
 }
 
-//TEST_CASE("Verify convert int to double", "int converted to double") {
-//	REQUIRE(convert_to_double(10, .5) == 5);
-//}
+TEST_CASE("Verify convert int to double", "int converted to double") 
+{
+	REQUIRE(convert_to_double(10, .5) == 5);
+}
 
-//TEST_CASE("Verify convert double to int", "double converted to int") {
-//	REQUIRE(convert_double_to_int(10.5) == 10);
-//	REQUIRE(convert_double_to_int(15.59849459494949) == 10);
-//}
+TEST_CASE("Verify convert double to int", "double converted to int") 
+{
+REQUIRE(convert_double_to_int(10.5) == 10);
+REQUIRE(convert_double_to_int(15.59849459494949) == 15);
+}
 
 TEST_CASE("verify get area of circle")
 {
