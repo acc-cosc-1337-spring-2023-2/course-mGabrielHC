3280bf82fb18f1d7f7e065cb9b17dc6d69464049
mGabrielHC <123131838+mGabrielHC@users.noreply.github.com>
2023-02-17 19:02:11 +0000

    Homework 3

--- src/homework/03_decisions/main.cpp
+++ src/homework/03_decisions/main.cpp
@@ -1,6 +1,44 @@
 //write include statements
+#include "decisions.h"
+#include<iostream>
+
+using std::cout;
+using std::cin;
 
 int main() 
 {
+	cout<<"MAIN MENU"<<"\n";
+
+	auto option = 0;
+
+	cout<<"Enter a menu option"<<"\n";
+	cout<<"1-Letter grade using if"<<"\n";
+	cout<<"2-Letter grade using switch"<<"\n";
+	cout<<"3-Exit"<<"\n";
+	cin>>option;
+
+	if(option == 1)
+	{
+		auto grade = 0;
+
+		cout<<"Enter a grade: ";
+		cin>>grade;
+
+		auto result = get_letter_grade_using_if(grade);
+		cout<<result<<"\n";
+	}
+
+	else if(option == 2)
+	{
+		auto grade = 0;
+
+		cout<<"Enter a grade: ";
+		cin>>grade;
+
+		auto result = get_letter_grade_using_switch(grade);
+		cout<<result<<"\n";
+	}
+	else(option == 3);
+
 	return 0;
 }
--- test/homework/03_decisions/03_decisions_tests.cpp
+++ test/homework/03_decisions/03_decisions_tests.cpp
@@ -1,7 +1,30 @@
 #define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
 #include "catch.hpp"
+#include "decisions.h"
 
 TEST_CASE("Verify Test Configuration", "verification") {
 	REQUIRE(true == true);
+}
+
+TEST_CASE("Verify get letter grade using if")
+{
+	REQUIRE(get_letter_grade_using_if(100) == "A");
+	REQUIRE(get_letter_grade_using_if(96) == "A");
+	REQUIRE(get_letter_grade_using_if(80) == "B");
+	REQUIRE(get_letter_grade_using_if(70) == "C");
+	REQUIRE(get_letter_grade_using_if(60) == "D");
+	REQUIRE(get_letter_grade_using_if(45) == "F");
+	REQUIRE(get_letter_grade_using_if(126) == "Invalid grade");
+}
+
+TEST_CASE("Verify get letter grade using switch")
+{
+	REQUIRE(get_letter_grade_using_switch(95) == "A");
+	REQUIRE(get_letter_grade_using_switch(85) == "B");
+	REQUIRE(get_letter_grade_using_switch(75) == "C");
+	REQUIRE(get_letter_grade_using_switch(65) == "D");
+	REQUIRE(get_letter_grade_using_switch(50) == "F");
+	REQUIRE(get_letter_grade_using_switch(126) == "Invalid grade");
+
 }
 
