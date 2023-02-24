fe19d9286a2971887bbd36d741461648f66b4f4e
mGabrielHC <123131838+mGabrielHC@users.noreply.github.com>
2023-02-24 06:53:43 +0000

    notes

--- src/homework/04_repetition/dna.cpp
+++ src/homework/04_repetition/dna.cpp
@@ -1,3 +1,15 @@
 //add include statements
-
+#include "dna.h"
 //add function(s) code here
+
+int factorial(int num)
+{
+    auto sum = 1;
+
+    while(num > 0)
+    {
+        sum *= num;
+        num--;
+    }
+   return sum;
+}
--- src/homework/04_repetition/main.cpp
+++ src/homework/04_repetition/main.cpp
@@ -1,7 +1,8 @@
 //write include statements
-
+#include "dna.h"
 //write using statements
-
+using std::cout;
+using std::cin;
 
 /*
 Write code that prompts user to enter 1 for Get GC Content, 
@@ -12,5 +13,41 @@ user enters a y or Y.
 */
 int main() 
 {
+	auto option = 0;
+
+	do
+	{
+		cout<<"1-Factorial"<<"\n";
+		cout<<"2-Greatest Common Divisor"<<"\n";
+		cout<<"3-Exit"<<"\n";
+		cin>>option;
+
+		if(option == 1)
+		{
+			auto num = 0;
+
+			cout<<"Enter a number: "<<"\n";
+			cin>>num;
+
+			num = factorial(num);
+			cout<<"The factorial of this number is: "<<num<<"\n";
+
+		}
+		else if(option == 3)
+		{
+			auto choise = 0;
+
+			cout<<"are you sure you want to exit? if yes pres 3."<<"\n";
+			cin>>choise;
+			if(choise == 3)
+			{
+				option++;
+			}
+
+		}
+		
+	} while (option != 4);
+
+
 	return 0;
 }
