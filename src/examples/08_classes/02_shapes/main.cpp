5a79a1ac826be58458a908910489dcd57b330099
mGabrielHC <123131838+mGabrielHC@users.noreply.github.com>
2023-03-28 23:12:35 +0000

    shape

--- src/examples/08_classes/02_shapes/main.cpp
+++ src/examples/08_classes/02_shapes/main.cpp
@@ -10,9 +10,23 @@ Create a Shape pointer of type Line
 Create vector of Shape pointers
 iterate with auto
 */
+using std::unique_ptr; 
+using std::make_unique; 
+using std::vector;
 
 int main() 
 {
+	vector<unique_ptr<Shape>> shapes;
+	shapes.push_back(make_unique<Circle>());
+	shapes.push_back(make_unique<Line>());
+
+	shapes[0]->draw();
+	shapes[1]->draw();
+
+	for(auto& shape: shapes)
+	{
+		shape->draw();
+	}
 
 	return 0;
 }
--- src/examples/08_classes/02_shapes/shape.cpp
+++ src/examples/08_classes/02_shapes/shape.cpp
@@ -1,4 +1,6 @@
-//shape.cpp
-
-
-void Shape:: draw()
+#include "shape.h"
+//class function/method implementation is optional
+void Shape::draw()
+{
+    std::cout<<"Shape...\n";
+}
