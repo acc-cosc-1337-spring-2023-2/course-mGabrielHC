fd959ebd429b6ff5c955612f4497d764dd6ad2c0
mGabrielHC <123131838+mGabrielHC@users.noreply.github.com>
2023-04-21 15:28:55 +0000

    notes update

--- test/examples/11_memory/11_memory_tests.cpp
+++ test/examples/11_memory/11_memory_tests.cpp
@@ -1,11 +1,12 @@
 #define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
 #include "catch.hpp"
 #include "ref_pointers.h"
+#include "my_vector.h"
 
 TEST_CASE("Verify Test Configuration", "verification") {
 	REQUIRE(true == true);
 }
-
+/*
 TEST_CASE("Test ref ptr params")
 {
 	int num1 = 1;
@@ -15,4 +16,28 @@ TEST_CASE("Test ref ptr params")
 
 	REQUIRE(num1 == 10);
 	REQUIRE(num2 == 20);
+}
+*/
+
+TEST_CASE("Test empty my vector initial size")
+{
+	Vector v(3);
+
+	REQUIRE(v.Size() == 0);
+}
+
+TEST_CASE("Test my vector capacity")
+{
+	Vector v(3);
+
+	REQUIRE(v.Capacity() == 3);
+}
+
+TEST_CASE("Test create Vector v1 from existing v")
+{
+	Vector v(3);
+	Vector v1 = v;
+
+	REQUIRE(v1.Size() == v.Size());
+	REQUIRE(v1.Capacity() == v.Capacity());
 }
